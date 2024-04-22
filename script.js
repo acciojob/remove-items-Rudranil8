bf14e6a8bae51f91040d8764444574ce16071b11
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-22 20:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//your JS code here. If required.
-// var dropdown=document.getElementById("colorSelect");
-// var button=document.querySelector("input[type="button"]");
-// button.addEventListener("click", function(){
-// 	var selectedIndex=dropdown.selectedIndex;
-// 	dropdown.remove(selectedIndex);
-// })
 var dropdown = document.getElementById("colorSelect");
 var button = document.querySelector('input[type="button"]');
 button.addEventListener("click", function(){
